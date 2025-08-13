69e0cfdb19729cc6e7bfad97feb9cf655ec593f7
Diptipathak2007 <diptipathak019@gmail.com>
2025-08-13 07:25:56 +0530

    tuples in typescript

---
+++ 02basics/myTupEnums.js
@@ -0,0 +1,7 @@
+//const user:(string|number)[]=["hc",1]
+var tUser;
+tUser = ["hc", 13123, true];
+var rgb = [255, 121, 134];
+var newUser = [112, "example@gmail.com"];
+newUser[1] = "hc.com";
+//newUser.push(true)
---
+++ 02basics/myTupEnums.ts
@@ -0,0 +1,8 @@
+//const user:(string|number)[]=["hc",1]
+let tUser:[string,number,boolean]
+tUser=["hc",13123,true]
+let rgb:[number,number,number]=[255,121,134]
+type User=[number,string]
+const newUser:User=[112,"example@gmail.com"]
+newUser[1]="hc.com"
+//newUser.push(true)
