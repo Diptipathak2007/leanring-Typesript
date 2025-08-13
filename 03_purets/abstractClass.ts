d95448348a2a8877d239dd3afdb8c481f66e3e4c
Diptipathak2007 <diptipathak019@gmail.com>
2025-08-13 22:04:51 +0530

    abstract class in typescript

---
+++ 03_purets/abstractClass.ts
@@ -0,0 +1,20 @@
+// abstract class TakePhoto{
+//     constructor(
+//         public cameramode:string,
+//         public filter:string
+//     ){}
+//     abstract getSepia():void;
+//     getREELtime():number{
+//         //complex calculation
+//         return 8
+//     }
+// }
+// class Instagram extends TakePhoto{
+//     constructor(public cameramode:string,public filter:string,public burst:number){
+//          super(cameramode,filter)
+//     }
+//     getSepia(): void {
+//         console.log("sepia");
+//     }
+// }
+//const divya=new Instagram("test","Test")
