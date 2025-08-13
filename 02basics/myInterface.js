8bef9f3c30bd73a38d8bfce8759e479ad7d2e734
Diptipathak2007 <diptipathak019@gmail.com>
2025-08-13 17:19:10 +0530

    interface vs type in typescript

--- 02basics/myInterface.js
+++ 02basics/myInterface.js
@@ -1,2 +1,3 @@
-var hitesh = { db_id: 22, email: "h@h.com", userID: 2211, startTrial: function () { return "trail started"; }, getCoupon: function (name) { return 19; } };
+var hitesh = { db_id: 22, email: "h@h.com", userID: 2211, startTrial: function () { return "trail started"; }, getCoupon: function (name) { return 19; }, githubToken: "23" };
 hitesh.email = "h@hc.com";
+//in an interface u can add new fields to an existing sequence and a type cannot be changed after created
--- 02basics/myInterface.ts
+++ 02basics/myInterface.ts
@@ -7,5 +7,13 @@ interface IUser{
     startTrial():string
     getCoupon(couponname:string):number
 }
-const hitesh:IUser={db_id:22,email:"h@h.com",userID:2211,startTrial:()=>{return "trail started"},getCoupon:(name:"hitesh")=>{return 19}}
+interface IUser{
+    githubToken:string,
+
+}
+interface Admin extends IUser{//extending an existing interface
+   role:"admin"|"ta"|"learner"
+}
+const hitesh:IUser={db_id:22,email:"h@h.com",userID:2211,startTrial:()=>{return "trail started"},getCoupon:(name:"hitesh")=>{return 19},githubToken:"23"}
 hitesh.email="h@hc.com"
+//in an interface u can add new fields to an existing sequence and a type cannot be changed after created
