814d182dc73f15c88391e6125705aca0202867e5
Diptipathak2007 <diptipathak019@gmail.com>
2025-08-13 17:56:07 +0530

    getters and setters in typescript

--- 03_purets/index.js
+++ 03_purets/index.js
@@ -8,16 +8,35 @@
 //         this.city=this.city
 //     }
 // }
-var pUser = /** @class */ (function () {
-    function pUser(email, name) {
+var piiUser = /** @class */ (function () {
+    function piiUser(email, name) {
         this.email = email;
         this.name = name;
+        this._coursecount = 1;
         this.city = "jaipur";
         this.email = email;
         this.email = name;
         this.city = this.city;
     }
-    return pUser;
+    Object.defineProperty(piiUser.prototype, "getAppleemail", {
+        get: function () { return "apple".concat(this.email); },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(piiUser.prototype, "coursecount", {
+        get: function () { return this._coursecount; },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(piiUser.prototype, "coursecounT", {
+        set: function (coursenum) { if (coursenum <= 1) {
+            throw new Error("course count should be more than one");
+        } },
+        enumerable: false,
+        configurable: true
+    });
+    piiUser.prototype.deleteToken = function () { console.log("Token deleted"); };
+    return piiUser;
 }());
-var dipti = new pUser("d@d.com", "dipti");
+var do0ipti = new piUser("d@d.com", "dipti");
 //dipti.city cannot be accessed
--- 03_purets/index.ts
+++ 03_purets/index.ts
@@ -8,14 +8,19 @@
 //         this.city=this.city
 //     }
 // }
-class piUser{
-    
-    private readonly city:string="jaipur"
-    constructor(public email:string,public name:string){
-        this.email=email
-        this.email=name
-        this.city=this.city
-    }
-}
-const doipti=new pUser("d@d.com","dipti")
+// //class piiUser{
+//     private _coursecount=1
+//     private readonly city:string="jaipur"
+//     constructor(public email:string,public name:string){
+//         this.email=email
+//         this.email=name
+//         this.city=this.city
+//     }
+//     get getAppleemail():string{return `apple${this.email}`}
+//     get coursecount():number{return this._coursecount}
+//     set coursecounT(coursenum){if(coursenum<=1){throw new Error("course count should be more than one")}}
+//     private deleteToken(){console.log("Token deleted")}
+// }
+
+//const do0ipti=new piUser("d@d.com","dipti")
 //dipti.city cannot be accessed
